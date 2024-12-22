be31c190a11366fe1f99a3d102f087dd69742f54
Muhammad Firas <muhammadfiras332@gmail.com>
2024-12-22 20:13:58 +0800

    Add files via upload

---
+++ Modul 5/Soal 1/Cylinder.java
@@ -0,0 +1,20 @@
+package praktikum5;
+
+public class Cylinder extends Shape {
+	private Double radius, height;
+	
+	public Cylinder(Double r, Double h) {
+		super("Cylinder");
+		radius = r;
+		height = h;
+	}
+	
+	public Double area() {
+		return Math.PI * (radius * radius) * height;
+	}
+	
+	public String toString() {
+		return super.toString() + " of radius " + radius + " and height " + height;
+	}
+}
+
